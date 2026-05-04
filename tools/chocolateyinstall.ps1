580d539e82664582a31fabc5653a99566ab07883
Mike Cooper <mike@equin.co.uk>
2026-05-04 12:59:06 +0100

    v1.13.16

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -2,7 +2,7 @@
 
 $packageName = $env:ChocolateyPackageName
 
-$url = 'https://download-electron.aircall.io/aircall-workspace/Aircall-Workspace-1.13.10-x64.msi'
+$url = 'https://download-electron.aircall.io/aircall-workspace/Aircall-Workspace-1.13.16-x64.msi'
 
 Write-Host "NOTE: This is an UNOFFICIAL, community-maintained Chocolatey package for Aircall Workspace."
 Write-Host "It is not maintained or endorsed by Aircall and may lag behind official releases."
@@ -14,7 +14,7 @@ $packageArgs = @{
 
   softwareName  = 'Aircall Workspace'
 
-  checksum      = '073A7B6F5198F60701F3260D24B9D4C4F952B1F9F5BEA0DCC16B93C6B1DEA12C'
+  checksum      = 'CF350FBE695B01333964C8DF3B9B82AFFD7BC9AE55EA554B9A628BA9164B7301'
   checksumType  = 'sha256'
 
   silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`"" # ALLUSERS=1 DISABLEDESKTOPSHORTCUT=1 ADDDESKTOPICON=0 ADDSTARTMENU=0
